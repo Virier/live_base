ccee03e528c77df3ab66645a65b7f244971b0b0a
zhangxiaowei <wiwiyeha@sina.com>
2022-06-20 17:18:29 +0800

    init

--- config/autoload/services.php
+++ config/autoload/services.php
@@ -19,7 +19,7 @@ return [
                 // 这个消费者要从哪个服务中心获取节点信息，如不配置则不会从服务中心获取节点信息
                 'registry' => [
                     'protocol' => 'nacos',
-                    'address' => '',  // http://172.17.213.40:8848
+                    'address' => '',
                 ],
                 // nodes配置可以不注册，为了确认是从consul获取的节点信息，这里先屏蔽
                 // 'nodes' => [
@@ -35,7 +35,7 @@ return [
             // nacos server url like https://nacos.hyperf.io, Priority is higher than host:port
             // 'url' => '',
             // The nacos host info
-            'host' => '', // 101.200.143.133
+            'host' => '',
             'port' => 8848,
             // The nacos account info
             'username' => 'nacos',
@@ -44,7 +44,7 @@ return [
                 'config' => null,
             ],
             'group_name' => 'DEFAULT_GROUP',
-            'namespace_id' => '', // a1525f83-7081-438d-a21a-e2fe8a28ce45
+            'namespace_id' => '',
             'heartbeat' => 5,
             'ephemeral' => true
         ],
