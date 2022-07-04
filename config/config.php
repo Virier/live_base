a3e5bd3fb1a3da24f583c34f46e6ee1826110bc8
zhangxiaowei <wiwiyeha@sina.com>
2022-07-04 14:56:38 +0800

    init

--- config/config.php
+++ config/config.php
@@ -15,7 +15,7 @@ use Psr\Log\LogLevel;
 return [
     'app_name' => env('APP_NAME', 'live_base'),
     'app_env' => env('APP_ENV', 'dev'),
-    'scan_cacheable' => env('SCAN_CACHEABLE', false), // 上线时需要设置为true
+    'scan_cacheable' => env('SCAN_CACHEABLE', true), // 上线时需要设置为true
     StdoutLoggerInterface::class => [ //日志根据不同的环境进行不同的配置
         'log_level' => [
             LogLevel::ALERT,
